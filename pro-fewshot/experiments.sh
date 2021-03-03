a573e2e4215d20a6a6807f0029b9deb721dccebf
maumruiz <mau.mruiz@hotmail.com>
2021-03-03 09:53:29 -0600

    Fix experiments shell scrpt

--- pro-fewshot/experiments.sh
+++ pro-fewshot/experiments.sh
@@ -1,8 +1,8 @@
-python train_protonet.py --way 5 --shot 1 --gpu 0 --model_type ConvNet
-python train_protonet.py --way 5 --shot 1 --gpu 0 --model_type ResNet
-python train_protonet.py --way 5 --shot 5 --gpu 0 --model_type ConvNet
-python train_protonet.py --way 5 --shot 5 --gpu 0 --model_type ResNet
-python train_protonet.py --way 20 --shot 1 --gpu 0 --model_type ConvNet
-python train_protonet.py --way 20 --shot 1 --gpu 0 --model_type ResNet
-python train_protonet.py --way 20 --shot 5 --gpu 0 --model_type ConvNet
-python train_protonet.py --way 20 --shot 5 --gpu 0 --model_type ResNet
+python train.py --way 5 --shot 1 --gpu 0 --model_type ConvNet
+python train.py --way 5 --shot 1 --gpu 0 --model_type ResNet
+python train.py --way 5 --shot 5 --gpu 0 --model_type ConvNet
+python train.py --way 5 --shot 5 --gpu 0 --model_type ResNet
+python train.py --way 20 --shot 1 --gpu 0 --model_type ConvNet
+python train.py --way 20 --shot 1 --gpu 0 --model_type ResNet
+python train.py --way 20 --shot 5 --gpu 0 --model_type ConvNet
+python train.py --way 20 --shot 5 --gpu 0 --model_type ResNet
