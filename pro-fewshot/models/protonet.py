bd67b924cd74abe8916d63415a391b4b58255eaa
maumruiz <mau.mruiz@hotmail.com>
2021-03-04 02:02:39 -0600

    Use new sampled batch to calculate prototypes

--- pro-fewshot/models/protonet.py
+++ pro-fewshot/models/protonet.py
@@ -1,5 +1,6 @@
+import torch
 import torch.nn as nn
-from util.metric import euclidean_metric
+from util.metric import euclidean_dist
 
 class ProtoNet(nn.Module):
 
@@ -18,8 +19,31 @@ class ProtoNet(nn.Module):
         else:
             raise ValueError('Encoder doesnt exist')
 
-    def forward(self, data_shot, data_query):
-        proto = self.encoder(data_shot)
-        proto = proto.reshape(self.args.shot, self.args.way, -1).mean(dim=0)
-        logits = euclidean_metric(self.encoder(data_query), proto) / self.args.temperature
+    def forward(self, data):
+        embeddings = self.encoder(data)
+
+        supp_fts = embeddings[:self.args.way*self.args.shot]
+        query_fts = embeddings[self.args.way*self.args.shot:]
+
+        # proto = supp_fts.reshape(self.args.way, self.args.shot, -1).mean(dim=1)
+        prototypes = self.compute_prototypes(supp_fts, self.args.way, self.args.shot)
+
+        logits = -euclidean_dist(query_fts, prototypes) / self.args.temperature
         return logits
+
+    def compute_prototypes(self, support: torch.Tensor, k: int, n: int) -> torch.Tensor:
+        """Compute class prototypes from support samples.
+
+        # Arguments
+            support: torch.Tensor. Tensor of shape (n * k, d) where d is the embedding
+                dimension.
+            k: int. "k-way" i.e. number of classes in the classification task
+            n: int. "n-shot" of the classification task
+
+        # Returns
+            class_prototypes: Prototypes aka mean embeddings for each class
+        """
+        # Reshape so the first dimension indexes by class then take the mean
+        # along that dimension to generate the "prototypes" for each class
+        class_prototypes = support.reshape(k, n, -1).mean(dim=1)
+        return class_prototypes
