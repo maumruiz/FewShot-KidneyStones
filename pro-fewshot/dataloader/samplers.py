3757487c8e6b1cf88835f1678b8ff57fbf80eb59
maumruiz <mau.mruiz@hotmail.com>
2021-03-04 01:55:16 -0600

    Add few shot task sampler

--- pro-fewshot/dataloader/samplers.py
+++ pro-fewshot/dataloader/samplers.py
@@ -30,3 +30,40 @@ class CategoriesSampler():
             batch = torch.stack(batch).t().reshape(-1)
             yield batch
 
+class FewShotSampler():
+    def __init__(self, label, n_episodes, n_way, n_shot, n_queries):
+        self.n_episodes = n_episodes
+        self.n_way = n_way
+        self.n_shot = n_shot
+        self.n_queries = n_queries
+
+        label = np.array(label)
+        self.m_ind = []
+        for i in range(max(label) + 1):
+            ind = np.argwhere(label == i).reshape(-1)
+            ind = torch.from_numpy(ind)
+            self.m_ind.append(ind)
+
+    def __len__(self):
+        return self.n_episodes
+    
+    def __iter__(self):
+        for i_batch in range(self.n_episodes):
+            batch = []
+            classes = torch.randperm(len(self.m_ind))[:5]
+
+            for c in classes:
+                l = self.m_ind[c]
+                pos = torch.randperm(len(l))[:5]
+                for im in l[pos]:
+                    batch.append(im)
+
+            for c in classes:
+                l = self.m_ind[c]
+                pos = torch.randperm(len(l))[:15]
+                for im in l[pos]:
+                    batch.append(im)
+
+            batch = torch.stack(batch)
+            yield batch
+
