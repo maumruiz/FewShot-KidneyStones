1d60e9a3f5c4f664097edf34c671aa167576b07f
maumruiz <mau.mruiz@gmail.com>
2021-03-11 07:20:18 -0800

    Experiment 3

--- pro-fewshot/experiments.sh
+++ pro-fewshot/experiments.sh
@@ -1,4 +1,2 @@
 python train.py --way 5 --shot 1 --train_way 30 --lr 0.001 --step_size 20 --gamma 0.5 --gpu 6 --backbone ConvNet --exp_num 3
-python train.py --way 5 --shot 1 --train_way 30 --lr 0.0001 --step_size 10 --gamma 0.2 --gpu 6 --backbone ConvNet --exp_num 4
 python train.py --way 5 --shot 5 --train_way 20 --lr 0.001 --step_size 20 --gamma 0.5 --gpu 6 --backbone ConvNet --exp_num 3
-python train.py --way 5 --shot 5 --train_way 20 --lr 0.0001 --step_size 10 --gamma 0.2 --gpu 6 --backbone ConvNet --exp_num 4
