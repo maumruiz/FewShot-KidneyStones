6ab706bbb6ef593cf72ae2a75fb79b3444fe653a
maumruiz <mau.mruiz@hotmail.com>
2021-03-11 22:46:10 -0600

    Experiments 9-12

---
+++ pro-fewshot/experiments2.sh
@@ -0,0 +1,8 @@
+python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "CTM" --ctm_blocks 1 --ctm_m_type avg --backbone ConvNet --exp_num 9
+python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "CTM" --ctm_blocks 2 --ctm_m_type avg --backbone ConvNet --exp_num 10
+python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "CTM" --ctm_blocks 1 --ctm_m_type avg --ctm_out_channels 32 --backbone ConvNet --exp_num 11
+python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "CTM" --ctm_blocks 2 --ctm_m_type avg --ctm_out_channels 32 --backbone ConvNet --exp_num 12
+python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "CTM" --ctm_blocks 1 --ctm_m_type avg --backbone ConvNet --exp_num 9
+python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "CTM" --ctm_blocks 2 --ctm_m_type avg --backbone ConvNet --exp_num 10
+python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "CTM" --ctm_blocks 1 --ctm_m_type avg --ctm_out_channels 32 --backbone ConvNet --exp_num 11
+python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "CTM" --ctm_blocks 2 --ctm_m_type avg --ctm_out_channels 32 --backbone ConvNet --exp_num 12
