5bb1579d16013fc96a50da97a4367e135ee80d1d
maumruiz <mau.mruiz@gmail.com>
2021-03-13 19:29:54 -0800

    Implement Resnet block in CTM

--- pro-fewshot/util/args_parser.py
+++ pro-fewshot/util/args_parser.py
@@ -39,6 +39,7 @@ def get_args():
     parser.add_argument('--ctm_block_type', type=str, default='ConvBlock', choices=['ConvBlock', 'ResBlock'])
     parser.add_argument('--ctm_m_type', type=str, default='fused', choices=['fused', 'avg'])
     parser.add_argument('--ctm_reduce_dims', action='store_true')
+    parser.add_argument('--ctm_split_blocks', action='store_true')
 
     return parser.parse_args()
 
@@ -57,6 +58,7 @@ def process_args(args):
         del args.ctm_block_type
         del args.ctm_m_type
         del args.ctm_reduce_dims
+        del args.ctm_split_blocks
 
     gmt = time.localtime() 
     timestmp = f'{gmt.tm_year}{gmt.tm_mon:02d}{gmt.tm_mday:02d}{gmt.tm_hour:02d}{gmt.tm_min:02d}{gmt.tm_sec:02d}'
