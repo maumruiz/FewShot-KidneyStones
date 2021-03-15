40293b702d9266f78103478c5de45fc66685eecd
maumruiz <mau.mruiz@gmail.com>
2021-03-15 13:15:07 -0700

    Add experiments 29-32

--- pro-fewshot/experiments2.sh
+++ pro-fewshot/experiments2.sh
@@ -1,9 +1,8 @@
-python train.py --way 5 --shot 5 --gpu 7 --modules "CTM" --ctm_blocks 4 --ctm_out_channels 32 --backbone ConvNet --exp_num 16
-python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "CTM" --ctm_blocks 3 --ctm_m_type avg --backbone ConvNet --exp_num 17
-python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "CTM" --ctm_blocks 4 --ctm_m_type avg --backbone ConvNet --exp_num 18
-python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "CTM" --ctm_blocks 3 --ctm_m_type avg --ctm_out_channels 32 --backbone ConvNet --exp_num 19
-python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "CTM" --ctm_blocks 4 --ctm_m_type avg --ctm_out_channels 32 --backbone ConvNet --exp_num 20
-python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "CTM" --ctm_blocks 3 --ctm_m_type avg --backbone ConvNet --exp_num 17
-python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "CTM" --ctm_blocks 4 --ctm_m_type avg --backbone ConvNet --exp_num 18
-python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "CTM" --ctm_blocks 3 --ctm_m_type avg --ctm_out_channels 32 --backbone ConvNet --exp_num 19
-python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "CTM" --ctm_blocks 4 --ctm_m_type avg --ctm_out_channels 32 --backbone ConvNet --exp_num 20
+python train.py --gpu 7 --way 5 --shot 1 --modules "CTM" --ctm_blocks 5 --ctm_split_blocks --ctm_reduce_dims --ctm_out_channels 32 --ctm_block_type ResBlock --backbone ConvNet --exp_num 29
+python train.py --gpu 7 --way 5 --shot 1 --train_way 30 --modules "CTM" --ctm_blocks 5 --ctm_reduce_dims --ctm_split_blocks --ctm_out_channels 32 --ctm_m_type avg --ctm_block_type ResBlock --backbone ConvNet --exp_num 30
+python train.py --gpu 7 --way 5 --shot 1 --modules "CTM" --ctm_blocks 3 --ctm_split_blocks --ctm_reduce_dims --ctm_out_channels 32 --ctm_block_type ResBlock --backbone ConvNet --exp_num 31
+python train.py --gpu 7 --way 5 --shot 1 --train_way 30 --modules "CTM" --ctm_blocks 3 --ctm_reduce_dims --ctm_split_blocks --ctm_out_channels 32 --ctm_m_type avg --ctm_block_type ResBlock --backbone ConvNet --exp_num 32
+python train.py --gpu 7 --way 5 --shot 5 --modules "CTM" --ctm_blocks 5 --ctm_split_blocks --ctm_reduce_dims --ctm_out_channels 32 --ctm_block_type ResBlock --backbone ConvNet --exp_num 29
+python train.py --gpu 7 --way 5 --shot 5 --train_way 20 --modules "CTM" --ctm_blocks 5 --ctm_reduce_dims --ctm_split_blocks --ctm_out_channels 32 --ctm_m_type avg --ctm_block_type ResBlock --backbone ConvNet --exp_num 30
+python train.py --gpu 7 --way 5 --shot 5 --modules "CTM" --ctm_blocks 3 --ctm_split_blocks --ctm_reduce_dims --ctm_out_channels 32 --ctm_block_type ResBlock --backbone ConvNet --exp_num 31
+python train.py --gpu 7 --way 5 --shot 5 --train_way 20 --modules "CTM" --ctm_blocks 3 --ctm_reduce_dims --ctm_split_blocks --ctm_out_channels 32 --ctm_m_type avg --ctm_block_type ResBlock --backbone ConvNet --exp_num 32
