fc510f6c37dab8217204476acb6535f8f40cb35e
maumruiz <mau.mruiz@gmail.com>
2021-03-18 06:47:12 -0700

    Experiments resnet18

--- pro-fewshot/experiments3.sh
+++
@@ -1,8 +0,0 @@
-python train.py --way 5 --shot 1 --gpu 5 --backbone ResNet12 --lr 0.001 --step_size 20 --gamma 0.5 --exp_num 33
-python train.py --way 5 --shot 1 --gpu 5 --backbone ResNet12 --lr 0.0001 --step_size 10 --gamma 0.2 --exp_num 34
-python train.py --way 5 --shot 1 --gpu 5 --backbone ResNet12 --lr 0.0001 --step_size 10 --gamma 0.5 --exp_num 35
-python train.py --way 5 --shot 1 --gpu 5 --backbone ResNet12 --lr 0.0001 --step_size 20 --gamma 0.5 --exp_num 35
-python train.py --way 5 --shot 5 --gpu 5 --backbone ResNet12 --lr 0.001 --step_size 20 --gamma 0.5 --exp_num 33
-python train.py --way 5 --shot 5 --gpu 5 --backbone ResNet12 --lr 0.0001 --step_size 10 --gamma 0.2 --exp_num 34
-python train.py --way 5 --shot 5 --gpu 5 --backbone ResNet12 --lr 0.0001 --step_size 10 --gamma 0.5 --exp_num 35
-python train.py --way 5 --shot 5 --gpu 5 --backbone ResNet12 --lr 0.0001 --step_size 20 --gamma 0.5 --exp_num 35
