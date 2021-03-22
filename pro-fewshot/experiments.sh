b551e36a7968dd99ecd4c206e27ef071f8c39394
maumruiz <mau.mruiz@gmail.com>
2021-03-22 11:43:11 -0700

    change gpu

--- pro-fewshot/experiments.sh
+++ pro-fewshot/experiments.sh
@@ -1,2 +1,2 @@
-python train.py --way 5 --shot 1 --gpu 5 --modules "ICN" --exp_num 99
-python train.py --way 5 --shot 5 --gpu 5 --modules "ICN" --exp_num 99
+python train.py --way 5 --shot 1 --gpu 2 --modules "ICN" --exp_num 99
+python train.py --way 5 --shot 5 --gpu 2 --modules "ICN" --exp_num 99
