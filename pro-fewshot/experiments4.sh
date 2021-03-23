6776ccbe533219c5bf363de37cfa8fe98806eefd
maumruiz <mau.mruiz@gmail.com>
2021-03-23 10:08:56 -0700

    change gpu

--- pro-fewshot/experiments4.sh
+++ pro-fewshot/experiments4.sh
@@ -1,8 +1,8 @@
-python train.py --way 5 --shot 1 --gpu 5 --backbone ResNet18 --lr 0.001 --step_size 20 --gamma 0.5 --exp_num 41
-python train.py --way 5 --shot 1 --gpu 5 --backbone ResNet18 --lr 0.0001 --step_size 10 --gamma 0.2 --exp_num 42
-python train.py --way 5 --shot 1 --gpu 5 --backbone ResNet18 --lr 0.0001 --step_size 10 --gamma 0.5 --exp_num 43
-python train.py --way 5 --shot 1 --gpu 5 --backbone ResNet18 --lr 0.0001 --step_size 20 --gamma 0.5 --exp_num 44
-python train.py --way 5 --shot 5 --gpu 5 --backbone ResNet18 --lr 0.001 --step_size 20 --gamma 0.5 --exp_num 41
-python train.py --way 5 --shot 5 --gpu 5 --backbone ResNet18 --lr 0.0001 --step_size 10 --gamma 0.2 --exp_num 42
-python train.py --way 5 --shot 5 --gpu 5 --backbone ResNet18 --lr 0.0001 --step_size 10 --gamma 0.5 --exp_num 43
-python train.py --way 5 --shot 5 --gpu 5 --backbone ResNet18 --lr 0.0001 --step_size 20 --gamma 0.5 --exp_num 44
+python train.py --way 5 --shot 1 --gpu 6 --backbone ResNet18 --lr 0.001 --step_size 20 --gamma 0.5 --exp_num 41
+python train.py --way 5 --shot 1 --gpu 6 --backbone ResNet18 --lr 0.0001 --step_size 10 --gamma 0.2 --exp_num 42
+python train.py --way 5 --shot 1 --gpu 6 --backbone ResNet18 --lr 0.0001 --step_size 10 --gamma 0.5 --exp_num 43
+python train.py --way 5 --shot 1 --gpu 6 --backbone ResNet18 --lr 0.0001 --step_size 20 --gamma 0.5 --exp_num 44
+python train.py --way 5 --shot 5 --gpu 6 --backbone ResNet18 --lr 0.001 --step_size 20 --gamma 0.5 --exp_num 41
+python train.py --way 5 --shot 5 --gpu 6 --backbone ResNet18 --lr 0.0001 --step_size 10 --gamma 0.2 --exp_num 42
+python train.py --way 5 --shot 5 --gpu 6 --backbone ResNet18 --lr 0.0001 --step_size 10 --gamma 0.5 --exp_num 43
+python train.py --way 5 --shot 5 --gpu 6 --backbone ResNet18 --lr 0.0001 --step_size 20 --gamma 0.5 --exp_num 44
