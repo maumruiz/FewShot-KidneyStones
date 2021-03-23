e6f832d0bb6ae57d369a7339a392c46750d216ba
maumruiz <mau.mruiz@gmail.com>
2021-03-22 23:48:10 -0700

    experiments with 30 and 20 way

--- pro-fewshot/experiments.sh
+++ pro-fewshot/experiments.sh
@@ -1,2 +1,2 @@
-python train.py --way 5 --shot 1 --train-way 30 --gpu 2 --modules "ICN" --exp_num 99
-python train.py --way 5 --shot 5 --train-way 20 --gpu 2 --modules "ICN" --exp_num 99
+python train.py --way 5 --shot 1 --train_way 30 --gpu 2 --modules "ICN" --exp_num 99
+python train.py --way 5 --shot 5 --train_way 20 --gpu 2 --modules "ICN" --exp_num 99
