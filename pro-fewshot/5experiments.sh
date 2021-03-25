1d4cf42b2683d9c545fdcafb95249ef48f9d911c
maumruiz <mau.mruiz@gmail.com>
2021-03-24 18:38:00 -0700

    experiments 50-51

---
+++ pro-fewshot/5experiments.sh
@@ -0,0 +1,4 @@
+python train.py --way 5 --shot 1 --gpu 0 --modules "ICN" --exp_num 50
+python train.py --way 5 --shot 5 --gpu 0 --modules "ICN" --exp_num 50
+python train.py --way 5 --shot 1 --train_way 30 --gpu 0 --modules "ICN" --exp_num 51
+python train.py --way 5 --shot 5 --train_way 20 --gpu 0 --modules "ICN" --exp_num 51
