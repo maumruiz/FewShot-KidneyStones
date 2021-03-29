acb03055f8145e4ab518945d5d9cf18cac469b53
maumruiz <mau.mruiz@gmail.com>
2021-03-29 14:24:00 -0700

    Experiments 64-65

--- pro-fewshot/62-63-experiments.sh
+++
@@ -1,4 +0,0 @@
-python train.py --way 5 --shot 5 --train_way 20 --gpu 6 --modules "ICN" --icn_reduction_set all --exp_num 63
-python train.py --way 5 --shot 5 --gpu 6 --modules "ICN" --icn_reduction_set all --exp_num 62
-python train.py --way 5 --shot 1 --train_way 30 --gpu 6 --modules "ICN" --icn_reduction_set all --exp_num 63
-python train.py --way 5 --shot 1 --gpu 6 --modules "ICN" --icn_reduction_set all --exp_num 62
