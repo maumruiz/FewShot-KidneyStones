305677866697760aa9596ea6bbb27292484c223e
maumruiz <mau.mruiz@gmail.com>
2021-05-19 07:40:55 -0700

    experiments

--- pro-fewshot/resnet-test.sh
+++
@@ -1,2 +0,0 @@
-python train.py --way 5 --shot 5 --backbone ResNet12 --gpu 5 --step_size 150 --train_way 20
-python train.py --way 5 --shot 1 --backbone ResNet12 --gpu 5 --step_size 150 --train_way 30
