048e4e617e9d6a9a35a716d6c1b6f53d154d7aad
maumruiz <mau.mruiz@gmail.com>
2021-05-25 17:14:38 -0700

    kidney stones data loader

---
+++ pro-fewshot/dataloader/kidney_stones.py
@@ -0,0 +1,80 @@
+import os.path as osp
+from PIL import Image
+
+from torch.utils.data import Dataset
+from torchvision import transforms
+import numpy as np
+
+THIS_PATH = osp.dirname(__file__)
+ROOT_PATH = osp.abspath(osp.join(THIS_PATH, '..'))
+IMAGE_PATH = osp.join(ROOT_PATH, 'data/kidneystones/images')
+SPLIT_PATH = osp.join(ROOT_PATH, 'data/kidneystones/split')
+
+class MiniImageNet(Dataset):
+    """ Usage: 
+    """
+    def __init__(self, setname, args):
+        csv_path = osp.join(SPLIT_PATH, setname + '.csv')
+        lines = [x.strip() for x in open(csv_path, 'r').readlines()][1:]
+
+        data = []
+        label = []
+        lb = -1
+
+        self.wnids = []
+
+        for l in lines:
+            name, wnid = l.split(',')
+            path = osp.join(IMAGE_PATH, name)
+            if wnid not in self.wnids:
+                self.wnids.append(wnid)
+                lb += 1
+            data.append(path)
+            label.append(lb)
+
+        self.data = data
+        self.label = label
+        self.num_class = len(set(label))
+
+        # Transformation
+        if args.backbone == 'ConvNet':
+            image_size = 84
+            self.transform = transforms.Compose([
+                transforms.Resize(92),
+                transforms.CenterCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(np.array([0.485, 0.456, 0.406]),
+                                     np.array([0.229, 0.224, 0.225]))
+            ])
+        elif 'ResNet' in args.backbone:
+            image_size = 80
+            self.transform = transforms.Compose([
+                transforms.Resize(92),
+                transforms.CenterCrop(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(np.array([x / 255.0 for x in [125.3, 123.0, 113.9]]), 
+                                     np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]))])
+        elif args.backbone == 'AmdimNet':
+            INTERP = 3
+            post_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            ])
+            self.transform = transforms.Compose([
+                transforms.Resize(146, interpolation=INTERP),
+                transforms.CenterCrop(128),
+                post_transform
+            ])
+        else:
+            raise ValueError('Non-supported Network Types. Please Revise Data Pre-Processing Scripts.')
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        path, label = self.data[i], self.label[i]
+        img_id = path.split('/')[-1].split('.')[0]
+        image = self.transform(Image.open(path).convert('RGB'))
+        return image, label, img_id
+
