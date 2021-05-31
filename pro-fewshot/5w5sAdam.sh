8fa9184b7cba26ebdb4a2fc3a08bd95827cda474
maumruiz <mau.mruiz@hotmail.com>
2021-05-31 12:33:13 -0500

    gpu

--- pro-fewshot/5w5sAdam.sh
+++ pro-fewshot/5w5sAdam.sh
@@ -1,4 +1,4 @@
-python train.py --way 5 --shot 5 --gpu 5 --max_epoch 100 --backbone AmdimNet --optimizer Adam --temperature 128 --step_size 10 --lr 0.0001 --init_weights pretrained/imagenet900.pth
-python train.py --way 5 --shot 5 --gpu 5 --max_epoch 100 --backbone AmdimNet --optimizer Adam --temperature 128 --step_size 10 --lr 0.0002 --init_weights pretrained/imagenet900.pth
-python train.py --way 5 --shot 5 --gpu 5 --max_epoch 100 --backbone AmdimNet --optimizer Adam --temperature 128 --step_size 20 --lr 0.0001 --init_weights pretrained/imagenet900.pth
-python train.py --way 5 --shot 5 --gpu 5 --max_epoch 100 --backbone AmdimNet --optimizer Adam --temperature 128 --step_size 20 --lr 0.0002 --init_weights pretrained/imagenet900.pth
+python train.py --way 5 --shot 5 --gpu 7 --max_epoch 100 --backbone AmdimNet --optimizer Adam --temperature 128 --step_size 10 --lr 0.0001 --init_weights pretrained/imagenet900.pth
+python train.py --way 5 --shot 5 --gpu 7 --max_epoch 100 --backbone AmdimNet --optimizer Adam --temperature 128 --step_size 10 --lr 0.0002 --init_weights pretrained/imagenet900.pth
+python train.py --way 5 --shot 5 --gpu 7 --max_epoch 100 --backbone AmdimNet --optimizer Adam --temperature 128 --step_size 20 --lr 0.0001 --init_weights pretrained/imagenet900.pth
+python train.py --way 5 --shot 5 --gpu 7 --max_epoch 100 --backbone AmdimNet --optimizer Adam --temperature 128 --step_size 20 --lr 0.0002 --init_weights pretrained/imagenet900.pth
