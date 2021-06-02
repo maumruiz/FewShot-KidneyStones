315943d6990ee231829836eafb1b2e1f276923c8
maumruiz <mau.mruiz@hotmail.com>
2021-06-02 18:54:39 -0500

    test with 1000 episodes

--- pro-fewshot/scripts/testKidney-Convnet.sh
+++ pro-fewshot/scripts/testKidney-Convnet.sh
@@ -1,6 +1,6 @@
-python test.py --way 5 --shot 5 --backbone ConvNet --dataset KidneyStones --test_epi 200 --gpu $1 --model_path pretrained/ConvNet-5w5s.pth
-python test.py --way 5 --shot 5 --backbone ConvNet --dataset KidneyStones --test_epi 200 --gpu $1 --model_path pretrained/ConvNet-5w5s20t.pth
-python test.py --way 5 --shot 20 --backbone ConvNet --dataset KidneyStones --test_epi 200 --gpu $1 --model_path pretrained/ConvNet-5w5s.pth
-python test.py --way 5 --shot 20 --backbone ConvNet --dataset KidneyStones --test_epi 200 --gpu $1 --model_path pretrained/ConvNet-5w5s20t.pth
-python test.py --way 5 --shot 50 --backbone ConvNet --dataset KidneyStones --test_epi 200 --gpu $1 --model_path pretrained/ConvNet-5w5s.pth
-python test.py --way 5 --shot 50 --backbone ConvNet --dataset KidneyStones --test_epi 200 --gpu $1 --model_path pretrained/ConvNet-5w5s20t.pth
+python test.py --way 5 --shot 5 --backbone ConvNet --dataset KidneyStones --test_epi 1000 --gpu $1 --model_path pretrained/ConvNet-5w5s.pth
+python test.py --way 5 --shot 5 --backbone ConvNet --dataset KidneyStones --test_epi 1000 --gpu $1 --model_path pretrained/ConvNet-5w5s20t.pth
+python test.py --way 5 --shot 20 --backbone ConvNet --dataset KidneyStones --test_epi 1000 --gpu $1 --model_path pretrained/ConvNet-5w5s.pth
+python test.py --way 5 --shot 20 --backbone ConvNet --dataset KidneyStones --test_epi 1000 --gpu $1 --model_path pretrained/ConvNet-5w5s20t.pth
+python test.py --way 5 --shot 50 --backbone ConvNet --dataset KidneyStones --test_epi 1000 --gpu $1 --model_path pretrained/ConvNet-5w5s.pth
+python test.py --way 5 --shot 50 --backbone ConvNet --dataset KidneyStones --test_epi 1000 --gpu $1 --model_path pretrained/ConvNet-5w5s20t.pth
