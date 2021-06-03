d99c14e9da1b652dacea84b15848a6785afa3f2c
maumruiz <mau.mruiz@hotmail.com>
2021-06-02 20:19:59 -0500

    test scripts

---
+++ pro-fewshot/scripts/testKidney-block5.sh
@@ -0,0 +1,9 @@
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 25 --model_path pretrained/Img1k-CD.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 25 --model_path pretrained/Img1k-CD.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 25 --model_path pretrained/Img1k-CD.pth
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 26 --model_path pretrained/Img1k-CD-ES.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 26 --model_path pretrained/Img1k-CD-ES.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 26 --model_path pretrained/Img1k-CD-ES.pth
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 27 --model_path pretrained/Img1k-CD-ES-ISIC.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 27 --model_path pretrained/Img1k-CD-ES-ISIC.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 27 --model_path pretrained/Img1k-CD-ES-ISIC.pth
