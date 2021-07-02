35c70096ee12e5d8962146bcc425a832b4238a25
maumruiz <mau.mruiz@gmail.com>
2021-07-02 14:58:51 -0700

    test yonathan

---
+++ pro-fewshot/scripts/testKidney-block7.sh
@@ -0,0 +1,24 @@
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 31 --model_path pretrained/Img900-Mini-CD.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 31 --model_path pretrained/Img900-Mini-CD.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 31 --model_path pretrained/Img900-Mini-CD.pth
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 32 --model_path pretrained/Img900-Mini-CUB.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 32 --model_path pretrained/Img900-Mini-CUB.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 32 --model_path pretrained/Img900-Mini-CUB.pth
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 33 --model_path pretrained/Img900-Mini-CUB-All.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 33 --model_path pretrained/Img900-Mini-CUB-All.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 33 --model_path pretrained/Img900-Mini-CUB-All.pth
+
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 31 --model_path pretrained/Img900-Mini-CD.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 31 --model_path pretrained/Img900-Mini-CD.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 31 --model_path pretrained/Img900-Mini-CD.pth
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 32 --model_path pretrained/Img900-Mini-CUB.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 32 --model_path pretrained/Img900-Mini-CUB.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 32 --model_path pretrained/Img900-Mini-CUB.pth
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 33 --model_path pretrained/Img900-Mini-CUB-All.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 33 --model_path pretrained/Img900-Mini-CUB-All.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 33 --model_path pretrained/Img900-Mini-CUB-All.pth
---
+++ pro-fewshot/scripts/testKidney-block8.sh
@@ -0,0 +1,24 @@
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 34 --model_path pretrained/Img900-All_ExcCUB2.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 34 --model_path pretrained/Img900-All_ExcCUB2.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 34 --model_path pretrained/Img900-All_ExcCUB2.pth
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 35 --model_path pretrained/Img1k-CUB2.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 35 --model_path pretrained/Img1k-CUB2.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 35 --model_path pretrained/Img1k-CUB2.pth
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 36 --model_path pretrained/Img1k-CUB2-All.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 36 --model_path pretrained/Img1k-CUB2-All.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 200 --gpu $1 --tag 36 --model_path pretrained/Img1k-CUB2-All.pth
+
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 34 --model_path pretrained/Img900-All_ExcCUB2.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 34 --model_path pretrained/Img900-All_ExcCUB2.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 34 --model_path pretrained/Img900-All_ExcCUB2.pth
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 35 --model_path pretrained/Img1k-CUB2.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 35 --model_path pretrained/Img1k-CUB2.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 35 --model_path pretrained/Img1k-CUB2.pth
+
+python test.py --way 5 --shot 5 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 36 --model_path pretrained/Img1k-CUB2-All.pth
+python test.py --way 5 --shot 20 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 36 --model_path pretrained/Img1k-CUB2-All.pth
+python test.py --way 5 --shot 50 --backbone AmdimNet --dataset KidneyStones --test_epi 1000 --gpu $1 --tag 36 --model_path pretrained/Img1k-CUB2-All.pth
