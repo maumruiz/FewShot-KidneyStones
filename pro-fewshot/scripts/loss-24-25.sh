3e892c4add0335f5e099cf91f2293c7211db80c7
maumruiz <mau.mruiz@gmail.com>
2021-08-10 15:28:35 -0700

    resnet 12 experiments

---
+++ pro-fewshot/scripts/loss-24-25.sh
@@ -0,0 +1,9 @@
+python train.py --gpu $1 --way 5 --shot 1 --train_way 5 --modules ICN_Loss --losses cross,suppicnn,queryicnn --query_protos --backbone ResNet12 --init_weights pretrained/ResNet-12-pretrained.pth --tag 24
+python train.py --gpu $1 --way 5 --shot 1 --train_way 30 --modules ICN_Loss --losses cross,suppicnn,queryicnn --query_protos --backbone ResNet12 --init_weights pretrained/ResNet-12-pretrained.pth --tag 24
+python train.py --gpu $1 --way 5 --shot 5 --train_way 5 --modules ICN_Loss --losses cross,suppicnn,queryicnn --query_protos --backbone ResNet12 --init_weights pretrained/ResNet-12-pretrained.pth --tag 24
+python train.py --gpu $1 --way 5 --shot 5 --train_way 20 --modules ICN_Loss --losses cross,suppicnn,queryicnn --query_protos --backbone ResNet12 --init_weights pretrained/ResNet-12-pretrained.pth --tag 24
+
+python train.py --gpu $1 --way 5 --shot 1 --train_way 5 --modules ICN_Loss --losses suppicnn,queryicnn --query_protos --backbone ResNet12 --init_weights pretrained/ResNet-12-pretrained.pth --tag 25
+python train.py --gpu $1 --way 5 --shot 1 --train_way 30 --modules ICN_Loss --losses suppicnn,queryicnn --query_protos --backbone ResNet12 --init_weights pretrained/ResNet-12-pretrained.pth --tag 25
+python train.py --gpu $1 --way 5 --shot 5 --train_way 5 --modules ICN_Loss --losses suppicnn,queryicnn --query_protos --backbone ResNet12 --init_weights pretrained/ResNet-12-pretrained.pth --tag 25
+python train.py --gpu $1 --way 5 --shot 5 --train_way 20 --modules ICN_Loss --losses suppicnn,queryicnn --query_protos --backbone ResNet12 --init_weights pretrained/ResNet-12-pretrained.pth --tag 25
